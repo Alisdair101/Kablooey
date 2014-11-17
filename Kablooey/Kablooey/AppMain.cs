0dd701d121f1340236873a5e05bc17cbbe9c7403
Alisdair101 <w007863c@student.staffs.ac.uk>
2014-11-17 15:31:16 +0000

    [+] Basic Game Scene Framework [AW]
    [+] Added and set up the Director.
    [-] Removed unnecessary code, GraphicsContext code and Render method.

---
+++ Kablooey/Kablooey/AppMain.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using Sce.PlayStation.HighLevel.UI;
+
+namespace Kablooey
+{
+	public class AppMain
+	{
+		private static Sce.PlayStation.HighLevel.GameEngine2D.Scene 	gameScene;
+		
+		public static void Main (string[] args)
+		{
+			Initialize ();
+			
+			//Game loop
+			bool quitGame = false;
+			while (quitGame) 
+			{
+				Update ();
+				
+				Director.Instance.Update();
+				Director.Instance.Render();
+				UISystem.Render();
+				
+				Director.Instance.GL.Context.SwapBuffers();
+				Director.Instance.PostSwap();
+			}
+			
+			Cleanup ();
+		}
+
+		public static void Initialize ()
+		{
+			//Set up director
+			Director.Initialize ();
+			
+			//Set game scene
+			gameScene = new Sce.PlayStation.HighLevel.GameEngine2D.Scene();
+			gameScene.Camera.SetViewFromViewport();
+			
+			//Run the scene.
+			Director.Instance.RunWithScene(gameScene, true);
+		}
+		
+		public static void Cleanup()
+		{
+			Director.Terminate();
+		}
+
+		public static void Update ()
+		{
+			
+		}
+	}
+}
