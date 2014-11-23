1e0a76026cf0ec89fb56dd328c22c540d3b48ebf
Alisdair101 <w007863c@student.staffs.ac.uk>
2014-11-23 20:59:22 +0000

    Revert "[+] Background Class Updated: Moving background [AW]"
    This reverts commit b536312bb9c01ccf843c0d4d7b3edeae7cb0ffb7.

--- Kablooey/Kablooey/Background.cs
+++ Kablooey/Kablooey/Background.cs
@@ -10,63 +10,24 @@ namespace Kablooey
 {
 	public class Background
 	{
-		private SpriteUV[] 	backgroundSprites;
+		private SpriteUV 	sprite;
 		private TextureInfo textureInfo;
-		
-		private float		width;
 		
 		public Background (Scene scene)
 		{
-			backgroundSprites = new SpriteUV[2];
-			
 			textureInfo = new TextureInfo("/Application/textures/background.png");
 			
-			backgroundSprites[0] = new SpriteUV(textureInfo);
-			backgroundSprites[0].Quad.S = textureInfo.TextureSizef;
+			sprite = new SpriteUV(textureInfo);
+			sprite.Quad.S = textureInfo.TextureSizef;
 			
-			backgroundSprites[1] = new SpriteUV(textureInfo);
-			backgroundSprites[1].Quad.S = textureInfo.TextureSizef;
+			sprite.Position = new Vector2(0.0f, 0.0f);
 			
-			Bounds2 bounds = backgroundSprites[0].Quad.Bounds2();
-			width = bounds.Point10.X;
-			
-			backgroundSprites[0].Position = new Vector2(0.0f, 0.0f);
-			backgroundSprites[1].Position = new Vector2(backgroundSprites[0].Position.X+width, 0.0f);
-			
-			foreach(SpriteUV sprite in backgroundSprites)
-			{
-				scene.AddChild(sprite);
-			}
+			scene.AddChild (sprite);
 		}
 		
 		public void Dispose()
 		{
 			textureInfo.Dispose();
-		}
-		
-		public void Update(float deltaTime)
-		{
-			backgroundSprites[0].Position = new Vector2(backgroundSprites[0].Position.X - 0.5f, backgroundSprites[0].Position.Y);
-			backgroundSprites[1].Position = new Vector2(backgroundSprites[1].Position.X - 0.5f, backgroundSprites[1].Position.Y);
-			
-			//Move the background.
-			if(backgroundSprites[0].Position.X < -width)
-			{
-				backgroundSprites[0].Position = new Vector2(backgroundSprites[1].Position.X+width, 0.0f);
-			}
-			else
-			{
-				backgroundSprites[0].Position = new Vector2(backgroundSprites[0].Position.X-1, 0.0f);	
-			}
-			
-			if(backgroundSprites[1].Position.X < -width)
-			{
-				backgroundSprites[1].Position = new Vector2(backgroundSprites[0].Position.X+width, 0.0f);
-			}
-			else
-			{
-				backgroundSprites[1].Position = new Vector2(backgroundSprites[1].Position.X-1, 0.0f);	
-			}
 		}
 	}
 }
