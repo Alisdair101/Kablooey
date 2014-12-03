1c58266a3549e50944034f3e3c27d74863a69c75
SMcewan <m008059c@student.staffs.ac.uk>
2014-12-03 10:00:46 +0000

    Normal Ship Class Added [SM]
    Added a class that creates the basic ship

---
+++ Kablooey/Kablooey/BaseShip.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Kablooey
+{
+	public class BaseShip : Ship
+	{
+		public BaseShip ()
+		{
+			textureInfo  = new TextureInfo("/Application/textures/base ship tex.png");
+		}
+	}
+}
+
