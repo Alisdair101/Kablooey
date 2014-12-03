673516318d14f0304fe864d4726a165aae15b2fc
SMcewan <m008059c@student.staffs.ac.uk>
2014-12-03 10:02:31 +0000

    Added Slow Ship Class [SM]
    Added Class to create the Slow Ship

---
+++ Kablooey/Kablooey/SlowShip.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Kablooey
+{
+	public class SlowShip : Ship
+	{
+		
+
+		public SlowShip ()
+		{
+			textureInfo  = new TextureInfo("/Application/textures/slowshiptex.png");
+			
+		}
+	}
+}
+
