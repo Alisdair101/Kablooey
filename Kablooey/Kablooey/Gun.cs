5fd25e7bbfd074c2a9a3354e1d4905e23b008c18
Alisdair101 <AlisdairTW@gmail.com>
2014-12-12 19:12:48 +0000

    [~] Correcting for Coding Standards [AW]
    [~] Correcting spelling mistakes made in sprite name, reticule to
    reticle
    [~] reticule.png texture file name changed to meet new spelling

--- Kablooey/Kablooey/Gun.cs
+++ Kablooey/Kablooey/Gun.cs
@@ -12,7 +12,7 @@ namespace Kablooey
 	public class Gun
 	{
 		private SpriteTile  gun;
-		private SpriteUV    reticule;
+		private SpriteUV    reticle;
 		
 		private TextureInfo gunTextureInfo;
 		private TextureInfo retTextureInfo;
@@ -32,16 +32,16 @@ namespace Kablooey
 			gun.Position = new Vector2(115.0f, 285.0f);
 			gun.TileIndex2D = new Vector2i(tileIndex, 0);
 			
-			retTextureInfo = new TextureInfo("/Application/textures/reticule.png");
-			reticule = new SpriteUV(retTextureInfo);
-			reticule.Quad.S = retTextureInfo.TextureSizef;
-			reticule.Position = new Vector2(200.0f, 300.0f);
+			retTextureInfo = new TextureInfo("/Application/textures/reticle.png");
+			reticle = new SpriteUV(retTextureInfo);
+			reticle.Quad.S = retTextureInfo.TextureSizef;
+			reticle.Position = new Vector2(200.0f, 300.0f);
 			
-			retAimPos = new Vector2(reticule.Position.X + 23.0f, reticule.Position.Y + 23.0f);
+			retAimPos = new Vector2(reticle.Position.X + 23.0f, reticle.Position.Y + 23.0f);
 			retSpeed = 10.0f;
 			
 			scene.AddChild(gun);
-			scene.AddChild(reticule);
+			scene.AddChild(reticle);
 		}
 		
 		public void Dispose()
@@ -56,23 +56,23 @@ namespace Kablooey
 			gamePadData = GamePad.GetData(0);
 			Controls ();
 			
-			retAimPos = new Vector2(reticule.Position.X + 23.0f, reticule.Position.Y + 23.0f);
+			retAimPos = new Vector2(reticle.Position.X + 23.0f, reticle.Position.Y + 23.0f);
 			
 			if(retAimPos.X >= 960)
 			{
-				reticule.Position = new Vector2(960 - 23, reticule.Position.Y);
+				reticle.Position = new Vector2(960 - 23, reticle.Position.Y);
 			}
 			if(retAimPos.X <= 115)
 			{
-				reticule.Position = new Vector2(115 - 23, reticule.Position.Y);
+				reticle.Position = new Vector2(115 - 23, reticle.Position.Y);
 			}
 			if(retAimPos.Y >= 544)
 			{
-				reticule.Position = new Vector2(reticule.Position.X, 544 - 23);
+				reticle.Position = new Vector2(reticle.Position.X, 544 - 23);
 			}
 			if(retAimPos.Y <= 0)
 			{
-				reticule.Position = new Vector2(reticule.Position.X, 0 - 23);
+				reticle.Position = new Vector2(reticle.Position.X, 0 - 23);
 			}
 		}
 		
@@ -98,7 +98,7 @@ namespace Kablooey
 		
 		public void setRetPosition(float X, float Y)
 		{
-			reticule.Position = new Vector2(reticule.Position.X + (X*retSpeed), reticule.Position.Y + (Y*retSpeed));
+			reticle.Position = new Vector2(reticle.Position.X + (X*retSpeed), reticle.Position.Y + (Y*retSpeed));
 		}
 		
 		public Vector2 getRetAimPosition()
